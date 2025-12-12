7427cc49fe6be13ff9d93d3ec51f8075e4708758
Andrea Postiglione <62989759+custom82@users.noreply.github.com>
2025-12-12 14:03:40 +0100

    Refactor macros and add error handling for functions
    Updated macros to include 'RAVEN_' prefix and added error checks for missing functions.

--- c/sph_luffa.h
+++ c/sph_luffa.h
@@ -31,20 +31,21 @@ typedef sph_luffa224_context sph_luffa256_context;
 typedef sph_luffa224_context sph_luffa384_context;
 typedef sph_luffa224_context sph_luffa512_context;
 
-/* Le funzioni dec32le_aligned e enc32le sono già definite in sph_types.h, quindi non ridefinirle */
-
-/* Macro per costanti */
+/* 1. PROTEGGI la definizione di SPH_C32 per evitare conflitti */
+#ifndef SPH_C32
 #define SPH_C32(x)    ((uint32_t)(x))
+#endif
 
+/* 2. RINOMINA le macro con un prefisso specifico (es. RAVEN_) */
 /* Macro per dichiarare tutte le variabili Vxx */
-#define DECLARE_VARS \
+#define RAVEN_DECLARE_VARS \
     uint32_t V00, V01, V02, V03, V04, V05, V06, V07; \
     uint32_t V10, V11, V12, V13, V14, V15, V16, V17; \
     uint32_t V20, V21, V22, V23, V24, V25, V26, V27; \
     uint32_t V30, V31, V32, V33, V34, V35, V36, V37;
 
 /* Macro per leggere lo stato */
-#define READ_STATE(state) \
+#define RAVEN_READ_STATE(state) \
     V00 = (state)->V[0];  V01 = (state)->V[1];  V02 = (state)->V[2];  V03 = (state)->V[3]; \
     V04 = (state)->V[4];  V05 = (state)->V[5];  V06 = (state)->V[6];  V07 = (state)->V[7]; \
     V10 = (state)->V[8];  V11 = (state)->V[9];  V12 = (state)->V[10]; V13 = (state)->V[11]; \
@@ -55,7 +56,7 @@ typedef sph_luffa224_context sph_luffa512_context;
     V34 = (state)->V[28]; V35 = (state)->V[29]; V36 = (state)->V[30]; V37 = (state)->V[31]
 
 /* Macro per scrivere lo stato */
-#define WRITE_STATE(state) \
+#define RAVEN_WRITE_STATE(state) \
     (state)->V[0] = V00;  (state)->V[1] = V01;  (state)->V[2] = V02;  (state)->V[3] = V03; \
     (state)->V[4] = V04;  (state)->V[5] = V05;  (state)->V[6] = V06;  (state)->V[7] = V07; \
     (state)->V[8] = V10;  (state)->V[9] = V11;  (state)->V[10] = V12; (state)->V[11] = V13; \
@@ -65,7 +66,22 @@ typedef sph_luffa224_context sph_luffa512_context;
     (state)->V[24] = V30; (state)->V[25] = V31; (state)->V[26] = V32; (state)->V[27] = V33; \
     (state)->V[28] = V34; (state)->V[29] = V35; (state)->V[30] = V36; (state)->V[31] = V37
 
-/* Prototipi delle funzioni */
+/* 3. VERIFICA/fornisci le funzioni di utilità mancanti */
+/* Se le funzioni non sono definite in sph_types.h, forniscile qui */
+#ifndef sph_dec32le_aligned
+/* Implementazione di fallback o errore di compilazione controllato */
+#error "sph_dec32le_aligned non definita. Verifica la tua versione di sph_types.h"
+#endif
+
+#ifndef sph_enc32le
+#error "sph_enc32le non definita. Verifica la tua versione di sph_types.h"
+#endif
+
+/* Alias per i nomi usati nel codice Ravencoin (se necessario) */
+#define dec32le_aligned sph_dec32le_aligned
+#define enc32le sph_enc32le
+
+/* Prototipi delle funzioni (rimangono invariati) */
 void sph_luffa224_init(void *cc);
 void sph_luffa224(void *cc, const void *data, size_t len);
 void sph_luffa224_close(void *cc, void *dst);
